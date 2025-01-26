88dfc40cac25cd2540a0a4fbc8ac26de7965923a
Oskari Mäki <oskari.maki@hotmail.com>
2025-01-26 07:59:20 +0200

    again

--- Assets/Scripts/BubbleCharacterController.cs
+++ Assets/Scripts/BubbleCharacterController.cs
@@ -57,6 +57,8 @@ public class BubbleCharacterController : Entity
         _controller = GetComponent<CharacterController>();
         //_camera.transform.SetParent(transform, false);
 
+        GameManager.instance.HealthValueText.text = CurrentHealth.ToString();
+
     }
 
     // Update
