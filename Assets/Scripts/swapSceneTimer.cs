8c67d5f288435abd408082dd2bd055012574271a
Mikey96r <mikkoex@gmail.com>
2025-01-26 04:58:21 +0200

    BuildSettings And Intro Scene script

---
+++ Assets/Scripts/swapSceneTimer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swapSceneTimer : MonoBehaviour
+{
+    public float timeToSwap;
+    private float m_timer;
+
+    private void Update()
+    {
+        m_timer += Time.deltaTime;
+        if (m_timer > timeToSwap)
+            Application.LoadLevel(2);
+        if(Input.GetKeyDown(KeyCode.Space))
+            Application.LoadLevel(2);
+    }
+}
